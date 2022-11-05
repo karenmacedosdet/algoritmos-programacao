c4540c928cdd20df70a144b99e5083b0a019a9c8
kdevmacedo <kdevmacedo@gmail.com>
2022-11-05 16:56:18 -0300

    Versao inicial do Projeto de Media

---
+++ a1-projeto2/cmake-build-debug/CMakeFiles/a1_projeto2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a1_projeto2.dir/main.cpp.o"
+  "a1_projeto2"
+  "a1_projeto2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a1_projeto2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ a1-projeto2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+    float nota1;
+    float nota2;
+    float media;
+
+    printf("Insira nota 1: \n");
+    scanf("%f", &nota1);
+
+    printf("Insira nota 2: \n");
+    scanf("%f", &nota2);
+
+    media = (nota1 + nota2) / 2;
+
+    printf("media = %f", media);
+
+    return 0;
+}
